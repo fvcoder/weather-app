832e6e41e9432a76d3710e447e6347c6dbec1b4b
Fernando Ticona <46249834+fvcoder@users.noreply.github.com>
2023-07-09 21:52:34 -0400

    clima view

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { IndexPage } from './router/index/index.component';
+import { ViewPage } from './router/view/view.component';
 
-const routes: Routes = [{ component: IndexPage, path: '' }];
+const routes: Routes = [
+  { component: IndexPage, path: '' },
+  {
+    component: ViewPage,
+    path: ':zone',
+  },
+];
 
 @NgModule({
-	imports: [
-		RouterModule.forRoot(routes, {
-			initialNavigation: 'enabledBlocking',
-		}),
-	],
-	exports: [RouterModule],
+  imports: [
+    RouterModule.forRoot(routes, {
+      initialNavigation: 'enabledBlocking',
+    }),
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { IndexPage } from './router/index/index.component';
+import { ViewPage } from './router/view/view.component';
 
 @NgModule({
-  declarations: [AppComponent, IndexPage],
+  declarations: [AppComponent, IndexPage, ViewPage],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, ReactiveFormsModule],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
